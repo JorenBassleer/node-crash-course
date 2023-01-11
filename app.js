5e8575473f41501db9c530f985101865b7ea2cd3
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-11 21:58:09 +0100

    further learned mongodb & nodejs. added start of api

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const Appliance = require('./models/appliances');
-const dbURI = 'mongodb+srv://JBassleerAdmin:7zMg2FaX0M9BdNA2@cluster0.9xeodoz.mongodb.net/?retryWrites=true&w=majority';
 
+const dbURI = process.env.dburi;
+console.log(dbURI);
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => app.listen(3000))
     .catch((error) => console.log('err'));
