0bf7d60f11600af841867e069c7ed8d7d3be0042
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-29 11:21:23 +0100

    converted type routes

---
+++ routes/type.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const router = express.router();
+const typeController = require('../controllers/type');
+
+module.exports = (app) => {
+  app.use('/type', router);
+  router.get('/', async (req, res) => {
+    try {
+      const brandRecords = await typeController.getAllTypes();
+      return res.status(200).json(brandRecords);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
+  router.get('/:id', async (req, res) => {
+    try {
+      const foundType = await typeController.findTypeById(req.params.id);
+      return res.status(200).json(foundType);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
+  router.post('/', async (req, res) => {
+    try {
+      const createdType = await typeController.createType(req.body);
+      return res.status(200).json(createdType);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
+  router.put('/:id', async (req, res) => {
+    try {
+      const updatedType = await typeController.updateType(req.params.id, req.body);
+      return res.status(200).json(updatedType);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deletedType = await typeController.deleteType(req.params.id);
+      return res.status(200).json(deletedType);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  });
+};
