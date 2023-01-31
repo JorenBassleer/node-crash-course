59c0e6d5eaca897bdcf5c3cba216bb7283f43590
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-01-31 19:39:11 +0100

    updated delete call & debugging update func

--- controllers/appliance/index.js
+++ controllers/appliance/index.js
@@ -14,11 +14,14 @@ const createAppliance = async (appliance) => {
   return newAppliance;
 };
 const updateAppliance = async (id, appliance) => {
-  const updatedAppliance = await Appliance.updateOne({
-    _id: id,
-    $set: appliance,
-  }).save();
-  return updatedAppliance;
+  const applianceToUpdate = Appliance.findOne({ _id: id });
+  console.log('here');
+  applianceToUpdate.name = appliance.name;
+  applianceToUpdate.details = appliance.details;
+  applianceToUpdate.type = appliance.type;
+  applianceToUpdate.brand = appliance.brand;
+  console.log('hereh 2', applianceToUpdate.save());
+  return applianceToUpdate.save();
 };
 // TODO: Add soft delete
 const deleteApplianceById = async (id) => {
--- routes/appliance.js
+++ routes/appliance.js
@@ -18,7 +18,6 @@ module.exports = (app) => {
   router.post('/', async (req, res) => {
     try {
       const createdAppliance = await applianceController.createAppliance(req.body);
-      console.log('createdAppliance: ', createdAppliance);
       return res.status(200).json(createdAppliance);
     } catch (error) {
       return res.status(500).json(error);
@@ -37,13 +36,14 @@ module.exports = (app) => {
   router.put('/:id', async (req, res) => {
     try {
       const updatedAppliance = await applianceController.updateAppliance(req.params.id, req.body);
+      console.log('updated Appliance in routes', updatedAppliance);
       return res.status(200).json(updatedAppliance);
     } catch (error) {
       return res.status(500).json(error);
     }
   });
   // TODO: Add soft delete
-  router.delete('/delete/:id', async (req, res) => {
+  router.delete('/:id', async (req, res) => {
     try {
       const deletedAppliance = await applianceController.deleteApplianceById(req.params.id);
       return res.status(200).json(deletedAppliance);
